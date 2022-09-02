21c22d7e8154725eb84b8e643e2df119240db31f
Henrik Roos Møller <hrm270800@gmail.com>
2022-09-02 12:07:14 +0200

    Added IsRented bool setter method with Timer

--- SurfBoardManager/Models/BoardPost.cs
+++ SurfBoardManager/Models/BoardPost.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Timers;
 
 namespace SurfBoardManager.Models
 {
@@ -14,6 +15,8 @@ namespace SurfBoardManager.Models
         public enum Type { Shortboard, Funboard, Fish, Longboard, SUP };
 
         public int Id { get; set; }
+
+        private static System.Timers.Timer _timer;
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", 
         ErrorMessage = "Må kun indeholde bogstaver(stort startbogstav) (A-Z), ingen specialtegn (*/.?) undtagen mellemrum.")]
@@ -53,10 +56,39 @@ namespace SurfBoardManager.Models
         [Display(Name = "Billede")]
         public string BoardImage { get; set; }
         public DateTime? RentalDate { get; set; }
-        public DateTime? RentalDateEnd { get; set; }
+        DateTime? rentalDateEnd;
+        public DateTime? RentalDateEnd 
+        {
+            get { return rentalDateEnd; }
+            set 
+            { rentalDateEnd = value; 
+                SetIsRentedStatus();
+            } 
+        }
 
         [Column(TypeName = "BIT")]
         [Display(Name = "Udlejet")]
         public bool IsRented { get; set; }
+
+        private void SetIsRentedStatus()
+        {
+            IsRented = true;
+
+            TimeSpan? interval = RentalDateEnd - RentalDate;
+            _timer = new System.Timers.Timer();
+            _timer.Interval = interval.Value.TotalMilliseconds;
+            _timer.AutoReset = false;
+            _timer.Enabled = true;
+
+            if (_timer_Elapsed == null)
+            {
+                _timer.Elapsed += _timer_Elapsed;
+            }
+        }
+
+        private void _timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            IsRented = false;
+        }
     }
 }
