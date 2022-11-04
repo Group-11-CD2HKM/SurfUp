a11a91463819b3541b2e9655b962a4d5863417e1
Daniel Madsen <steamblacksej2@gmail.com>
2022-11-04 13:07:23 +0100

    Blazor WASM created. Started work on interface service

---
+++ SurfBoardManagerBlazorWASM/Services/IBoardPostService.cs
@@ -0,0 +1,11 @@
+﻿using SurfUpLibary;
+
+namespace SurfBoardManagerBlazorWASM.Services
+{
+    public interface IBoardPostService
+    {
+        public List<BoardPost> GetAllUnrentedBoardPosts();
+
+        public void RentBoard(BoardPost boardPost);
+    }
+}
