347d717375908d6b9f70879e2ae7165d289edb34
Daniel Madsen <steamblacksej2@gmail.com>
2022-11-08 09:54:32 +0100

    Merge stuffs.

---
+++ SurfBoardManagerBlazorWASM/Services/BoardPostService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using SurfUpLibary;
+
+namespace SurfBoardManagerBlazorWASM.Services
+{
+    public class BoardPostService : IBoardPostService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
+        public BoardPostService(HttpClient http, NavigationManager navManager)
+        {
+            _httpClient = http;
+            _navigationManager = navManager;
+        }
+
+        public List<BoardPost> GetAllUnrentedBoardPosts()
+        {
+
+        }
+
+        public void RentBoard(BoardPost boardPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
