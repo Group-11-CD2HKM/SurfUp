7e9b6a483bcb7ef5dab5d5a7e3a8e12351ec5097
Kelvin Tarp Nielsen <kelvin@newtsdash.co.uk>
2022-11-14 12:42:13 +0100

    nearly works

--- SurfBoardBlazorWASM/Client/Services/BoardPostService.cs
+++ SurfBoardBlazorWASM/Client/Services/BoardPostService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using SurfUpLibary;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace SurfBoardBlazorWASM.Client.Services
@@ -8,18 +10,28 @@ namespace SurfBoardBlazorWASM.Client.Services
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly NavigationManager _navigationManager;
-        public BoardPostService(IHttpClientFactory httpClientFactory, NavigationManager navManager)
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        public BoardPostService(IHttpClientFactory httpClientFactory, NavigationManager navManager, AuthenticationStateProvider authenticationStateProvider)
         {
             _httpClientFactory = httpClientFactory;
             _navigationManager = navManager;
+            _authenticationStateProvider = authenticationStateProvider;
         }
 
         public async Task<List<BoardPost>> GetAllUnrentedBoardPosts()
         {
-            HttpClient httpClient = _httpClientFactory.CreateClient("SurfBoardBlazorWASM.PublicServerAPI");
-
-            var bordPosts = await httpClient.GetFromJsonAsync<List<BoardPost>>("api/Boards") ?? new List<BoardPost>();
-            return bordPosts;
+            List<BoardPost> boardPosts;
+            var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                HttpClient httpClient = _httpClientFactory.CreateClient("SurfBoardBlazorWASM.ServerAPI");
+                boardPosts = await httpClient.GetFromJsonAsync<List<BoardPost>>("api/v2.0/Boards") ?? new List<BoardPost>();
+            } else
+            {
+                HttpClient httpClient = _httpClientFactory.CreateClient("SurfBoardBlazorWASM.PublicServerAPI");
+                boardPosts = await httpClient.GetFromJsonAsync<List<BoardPost>>("api/Boards") ?? new List<BoardPost>();
+            }
+            return boardPosts;
         }
 
         public void RentBoard(BoardPost boardPost)
