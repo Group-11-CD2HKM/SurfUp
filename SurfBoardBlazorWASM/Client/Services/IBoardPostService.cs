8ddaeedf52e6a197d624b9329f48d6942109dc95
Kelvin Tarp Nielsen <kelvin@newtsdash.co.uk>
2022-11-16 09:35:56 +0100

    Implementation!

--- SurfBoardBlazorWASM/Client/Services/IBoardPostService.cs
+++ SurfBoardBlazorWASM/Client/Services/IBoardPostService.cs
@@ -6,6 +6,6 @@ namespace SurfBoardBlazorWASM.Client.Services
     {
         public Task<List<BoardPost>> GetAllUnrentedBoardPosts();
 
-        public void RentBoard(BoardPost boardPost);
+        public void RentBoard(BoardPost boardPost, int days);
     }
 }
